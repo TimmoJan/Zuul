bae76d0ca0d16389f80bc7c0dc36b90201bd3606
TimmoJan <58556388+TimmoJan@users.noreply.github.com>
2020-01-08 21:21:00 +0100

    Add files via upload

---
+++ zuul-bad/__SHELL1.java
@@ -0,0 +1,10 @@
+
+public class __SHELL1 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("C:\\Users\\timui\\OneDrive\\Documenten\\School\\Periode 2\\OOP1\\Projects\\chapter08\\zuul-bad");
+final Game game1 = (Game)__bluej_runtime_scope.get("game1");
+
+
+game1.play();
+
+}}
